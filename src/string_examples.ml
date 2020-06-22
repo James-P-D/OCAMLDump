bce7d7bf5b69f279ad320c1a4e468264b9798d75
James Dorrington <jdorrington@hotmail.com>
2020-06-22 15:47:16 +0100

    Adding string_examples.ml

---
+++ src/string_examples.ml
@@ -0,0 +1,35 @@
+(* We can declare strings *)
+let some_string = "This is a string\n";;
+
+(* We can get the zeroth-indexed character from a string *)
+let x = some_string.[0];;
+
+(* We can get the length of strings *)
+let string_length = String.length(some_string);;
+
+(* We can get the substring at index 5 for 4 characters *)
+let substring = String.sub some_string 5 4;;
+
+(* We can get the first index of a char in a string *)
+let index = String.rindex some_string 'a';;
+
+(* We can declare characters*)
+let some_char = 'x';;
+
+(* We can get the ASCII code for characters *)
+let ascii_code = Char.code(some_char);;
+
+(* We can get the char from the ASCII code *)
+let letter_from_code = Char.chr(65);;
+
+(* We can convert to lowercase *)
+let lowercase_a = Char.lowercase_ascii(letter_from_code);;
+
+(* We can convert to uppercase *)
+let uppercase_a = Char.uppercase_ascii(lowercase_a);;
+
+(* We can compare characters. (Function returns an int, zero if equal, 32 in our case) *)
+let compare_char = Char.compare 'a' 'A';;
+
+(* We can compare characters for equality. *)
+let equal_char = Char.equal 'a' 'a';;
