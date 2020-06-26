74d356292509478e28c42df0f61b9cb5d86bfe6e
James Dorrington <jdorrington@hotmail.com>
2020-06-26 10:56:56 +0100

    Updates for user input and for computer randomly choosing rock/paper/scissors

--- src/rock_paper_scissors.ml
+++ src/rock_paper_scissors.ml
@@ -1,19 +1,34 @@
 type rps_type = Rock | Paper | Scissors | Exit;;
 
-let read_char() =
-  print_string "Enter a string\n";
+let char_to_enum c =
+  match c with
+  | 'r' -> Rock
+  | 'p' -> Paper
+  | 's' -> Scissors
+  | c -> Exit;;
+
+let int_to_enum n =
+  match n with
+  | 0 -> Rock
+  | 1 -> Paper
+  | n -> Scissors;;
+
+let rec read_user_choice() =
+  print_endline "Enter (r)ock, (p)aper, (s)cissors, or e(x)it.";
   let str = input_line stdin in
-  print_string ("length is " ^ string_of_int(String.length(str)) ^ "\n");
-  if (String.length(str) > 0) then str.[0] else read_char();; 
+  if ((String.length(str) > 0) && ((str.[0] == 'r') || (str.[0] == 'p') || (str.[0] == 's') || (str.[0] == 'x'))) then char_to_enum(str.[0]) else read_user_choice();; 
   
 let read_user_input() =
-  print_string "before c\n";
-  let c = ref ' ' in
-  print_string "after c\n";
-  print_string "Enter (r)ock, (p)aper, (s)cissors, or e(x)it.\n";
-  c := read_char();
-  print_char !c;
-  print_string ", ok!\n";
-  c;;
+  let user_choice = ref Exit in
+  user_choice := read_user_choice();
+  user_choice;;
 
-read_user_input()
+let get_computer_choice() = 
+  let n = Random.int 3 in
+  int_to_enum n;;
+
+let rps() = 
+  Random.self_init();
+  get_computer_choice();; 
+
+rps();;
