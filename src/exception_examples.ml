4f415463ef38e52dcd4d8f9ec82c5df89bfc8a0a
James Dorrington <jdorrington@hotmail.com>
2020-06-29 10:12:40 +0100

    Adding example of throwing Exception in OCaml

--- src/exception_examples.ml
+++ src/exception_examples.ml
@@ -1,9 +1,19 @@
 (* We can catch exceptions *)
 let divider a b = try a / b with Division_by_zero -> -1;;
 
-let x = divider 10 2;; (" x = 5 ")
-let y = divider 10 0;; (" x = -1 ")
+let x = divider 10 2;; (* x = 5 *)
+let y = divider 10 0;; (* x = -1 *)
 
+(* We can create our own exceptions *)
+
+exception OddNumberException of string;;
+
+let double_if_even n =
+  if ((n mod 2) == 0) then (n * 2)
+  else raise (OddNumberException (string_of_int(n) ^ " is not an even number"));;
+  
+let works_ok = double_if_even 2;; (* val works_ok : int = 4 *)
+let fails = double_if_even 3;;    (* Exception: OddNumberException "3 is not an even number". *)
 
 (* We can also match on multiple different exceptions by using guards ('|') *)
 (*
